b16017e251e5f8c6a9adafbc8cee8adca015dfac
Roman Behma <begmaroman@gmail.com>
2020-11-03 19:29:22 +0200

    Revert "Allocate Fewer Large Slices  (#7698)"
    This reverts commit 40368bed

--- beacon-chain/core/epoch/precompute/slashing.go
+++ beacon-chain/core/epoch/precompute/slashing.go
@@ -26,20 +26,18 @@ func ProcessSlashingsPrecompute(state *stateTrie.BeaconState, pBal *Balance) err
 	minSlashing := mathutil.Min(totalSlashing*params.BeaconConfig().ProportionalSlashingMultiplier, pBal.ActiveCurrentEpoch)
 	epochToWithdraw := currentEpoch + exitLength/2
 
+	vs := state.ValidatorsReadOnly()
 	var hasSlashing bool
 	// Iterate through validator list in state, stop until a validator satisfies slashing condition of current epoch.
-	err := state.ReadFromEveryValidator(func(idx int, val *stateTrie.ReadOnlyValidator) error {
-		if val == nil {
+	for _, v := range vs {
+		if v == nil {
 			return errors.New("nil validator in state")
 		}
-		correctEpoch := epochToWithdraw == val.WithdrawableEpoch()
-		if val.Slashed() && correctEpoch {
+		correctEpoch := epochToWithdraw == v.WithdrawableEpoch()
+		if v.Slashed() && correctEpoch {
 			hasSlashing = true
+			break
 		}
-		return nil
-	})
-	if err != nil {
-		return err
 	}
 	// Exit early if there's no meaningful slashing to process.
 	if !hasSlashing {

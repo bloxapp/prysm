13a82aa2171cc3d27e293e0a6979c73e5934ac9e
Roman Behma <begmaroman@gmail.com>
2020-11-03 19:29:29 +0200

    Revert "Continue In ProcessPreGenesisDeposits (#7678)"
    This reverts commit d2ae1b92

--- beacon-chain/core/blocks/deposit.go
+++ beacon-chain/core/blocks/deposit.go
@@ -33,10 +33,8 @@ func ProcessPreGenesisDeposits(
 	for _, deposit := range deposits {
 		pubkey := deposit.Data.PublicKey
 		index, ok := beaconState.ValidatorIndexByPubkey(bytesutil.ToBytes48(pubkey))
-		// In the event of the pubkey not existing, we continue processing the other
-		// deposits.
 		if !ok {
-			continue
+			return beaconState, nil
 		}
 		balance, err := beaconState.BalanceAtIndex(index)
 		if err != nil {

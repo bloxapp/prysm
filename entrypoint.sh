d4b023ccc73ff7b91cc27ce908c927b34329ff3c
systemblox <systemblox@blox.io>
2021-01-15 13:03:02 +0200

    Reconfigured endpoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /prysm
 
-/usr/bin/bazel run //beacon-chain:beacon-chain -- --datadir=/data --http-web3provider=http://ethereum1-mainnet.blockchain.svc.k8.stage.bloxinternal.com:8545 --rpc-host=0.0.0.0 --rpc-port=14000 --grpc-gateway-host=0.0.0.0 --grpc-gateway-port=13500 --monitoring-host=0.0.0.0 --monitoring-port=16668 --p2p-max-peers=500 --mainnet --accept-terms-of-use --head-sync --verbosity=debug --p2p-host-dns=eth2-prysm-mainnet-ext.stage.bloxinfra.com --p2p-tcp-port=15000 --p2p-udp-port=16000 --subscribe-all-subnets
+/usr/bin/bazel run //beacon-chain:beacon-chain -- --datadir=/data --http-web3provider=http://ethereum1-mainnet.blockchain:8545 --rpc-host=0.0.0.0 --rpc-port=14000 --grpc-gateway-host=0.0.0.0 --grpc-gateway-port=13500 --monitoring-host=0.0.0.0 --monitoring-port=16668 --p2p-max-peers=500 --mainnet --accept-terms-of-use --head-sync --verbosity=debug --p2p-host-dns=eth2-prysm-mainnet-ext.stage.bloxinfra.com --p2p-tcp-port=15000 --p2p-udp-port=16000 --subscribe-all-subnets
